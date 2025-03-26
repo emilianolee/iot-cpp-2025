8bdf5a84182cdfcad1875a64258d7ec35f3c6de2
emilianolee <yechan5172@daum.net>
2025-03-26 17:31:17 +0900

    C++과 DB연동 커밋

--- DB/DB.cpp
+++ DB/DB.cpp
@@ -1,26 +1,29 @@
-#include <iostream>
-#include <mysql/jdbc.h>
-
-using namespace std;
-using namespace sql;
-
-int main()
-{
-	try {
-		mysql::MySQL_Driver* driver;
-		Connection* con;
-
-		driver = mysql::get_mysql_driver_instance();
-
-		con = driver->connect("tcp://127.0.0.1:3306", "madang", "12345");
-
-		con->setSchema("madang");
-
-		cout << "Connection Successful!!" << endl;
-	}
-	catch (SQLException& e) {
-		cerr << "MySQL Connection Failed!!" << e.what() << endl;
-	}
-
-	return 0;
-}
+///*
+//	DB ¿¬°á
+//*/
+//
+//#include <iostream>
+//#include <mysql/jdbc.h>
+//
+//using namespace std;
+//using namespace sql;
+//
+//int main()
+//{
+//	const string server = "tcp://127.0.0.1:3306";
+//	const string username = "root";
+//	const string password = "12345";
+//	const string database = "testDB";
+//
+//	try {
+//		mysql::MySQL_Driver* driver = mysql::get_mysql_driver_instance();
+//		unique_ptr<Connection> conn(driver->connect(server, username, password));
+//		conn->setSchema(database);
+//		cout << "MySQL Connection Success:)" << endl;
+//	}
+//	catch (SQLException& e) {
+//		cout << "MySQL Connection Fail:(" << e.what() << endl;
+//	}
+//
+//	return 0;
+//}
