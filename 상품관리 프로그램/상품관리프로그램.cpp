1d6a324611fa0cd3b04a4c152726f09e728aa4b7
emilianolee <yechan5172@daum.net>
2025-04-07 09:15:59 +0900

    상품관리 프로그램 커밋

--- 상품관리 프로그램/상품관리프로그램.cpp
+++ 상품관리 프로그램/상품관리프로그램.cpp
@@ -1,53 +1,83 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include <iostream>
+﻿#include <iostream>
 using namespace std;
 
+// Product 클래스(부모 클래스)
 class Product {
+private:
+    string id;
+    int price;
+    string producer;
 public:
-	string id;
-	int price;
-	string producer;
-	Product(string pid, int pprice, string pproducer) {
-		id = pid;
-		producer = pproducer;
-		//id = new char[strlen(pid) + 1];
-		//strcpy(id, pid);
-		price = pprice;
-		//producer = new char[strlen(pproducer) + 1];
-		//strcpy(producer, pproducer);
-	}
-	~Product() {
-		//delete[] id;
-		//delete[] producer;
-	}
+    Product(string pid, int pprice, string pproducer) {
+        id = pid;
+        producer = pproducer;
+        price = pprice;
+    }
+
+    void showInfo() {
+        cout << "상품 ID: " << id << endl;
+        cout << "가격: " << price << endl;
+        cout << "제조사: " << producer << endl;
+    }
 };
 
+// Book 클래스(자식 클래스)
 class Book : public Product {
 private:
-	int BookID;
+    string bookTitle;
 public:
-	Book(string id, int price, string producer, int BookID);
+    Book(string pid, int pprice, string pproducer, string pbookTitle) : Product(pid, pprice, pproducer) {
+        bookTitle = pbookTitle;
+    }
 
-	void showBook();
+    void showBookInfo() {
+        cout << "[책 정보]" << endl;
+        showInfo();
+        cout << "책 제목: " << bookTitle << endl << endl;
+    };
 };
 
-void Book::showBook() {
-std:cout << "å ID: " << BookID << std::endl;
-}
-
 class HandPhone : public Product {
+private:
+    string phoneName;
+public:
+    HandPhone(string pid, int pprice, string pproducer, string pphoneName) : Product(pid, pprice, pproducer) {
+        phoneName = pphoneName;
+    }
 
+    void showPhoneInfo() {
+        cout << "[휴대폰 정보]" << endl;
+        showInfo();
+        cout << "휴대폰 기종: " << phoneName << endl << endl;
+    }
 };
 
 class Computer : public Product {
+private:
+    string comName;
+public:
+    Computer(string pid, int pprice, string pproducer, string pcomName) : Product(pid, pprice, pproducer) {
+        comName = pcomName;
+    }
 
+    void showComputerInfo() {
+        cout << "[컴퓨터 정보]" << endl;
+        showInfo();
+        cout << "컴퓨터 상품명: " << comName << endl << endl;
+    }
 };
 
 
 
 int main()
 {
+    Book b("B001", 15000, "(주)능률", "토익만세");
+    HandPhone h("H001", 990000, "Apple", "IPhone 16");
+    Computer c("C001", 1200000, "Samsung", "삼성최고노트북");
 
+    b.showBookInfo();
+    h.showPhoneInfo();
+    c.showComputerInfo();
 
-	return 0;
+    return 0;
 }
